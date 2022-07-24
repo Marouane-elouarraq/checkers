ca32eef9af890d155c7998989c5c7dbd15ab9118
Sakuragi-crypto <108550029+Sakuragi-crypto@users.noreply.github.com>
2022-07-24 21:29:40 +0200

    jumping and king movement

---
+++ checkers/game.py
@@ -0,0 +1,31 @@
+import pygame
+from .constants import RED, WHITE
+from .board import Board
+
+
+class Game:
+    def __init__(self, win):
+        self._init()
+        self.win = win
+
+    def update(self):
+        self.board.draw(self.win)
+        pygame.display.update()
+
+    def _init(self):
+        self.selected = None
+        self.board = Board()
+        self.turn = RED
+        self.valid_moves = {}
+
+    def reset(self):
+        self._init()
+
+    def select(self, row, col):
+        if self.selected:
+            result = self._move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+
+    def _move(self, row, col):
